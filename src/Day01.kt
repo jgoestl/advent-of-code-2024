6a8786da1fae577d90cc16e70a057118ca7d3016
julian <Em14aAb31h461t4>
2024-12-01 14:46:13 +0100

    Day 01 Part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,24 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val listA = ArrayList<String>()
+        val listB = ArrayList<String>()
+        val listDistances = ArrayList<Int>()
+
+        input.stream().map { it.split("\\s+".toRegex()) }.forEach {
+            listA.add(it[0])
+            listB.add(it[1])
+        }
+
+        listA.sort()
+        listB.sort()
+
+
+        for (i in input.indices) {
+            val distance = listA[i].toInt() - listB[i].toInt()
+            listDistances.add(Math.abs(distance))
+        }
+
+        return listDistances.sum()
     }
 
     fun part2(input: List<String>): Int {
@@ -8,14 +26,14 @@ fun main() {
     }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    //check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
-    part2(input).println()
+    //part2(input).println()
 }
