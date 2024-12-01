9c27ed1afa64ed62001b7d828fbfce52308a8cca
julian <Em14aAb31h461t4>
2024-12-01 15:05:26 +0100

    Day 01 Part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,28 +1,54 @@
+import kotlin.math.abs
+
 fun main() {
-    fun part1(input: List<String>): Int {
+    fun part1(input: Array<ArrayList<String>>): Int {
+        val listDistances = ArrayList<Int>()
+
+        val listA = input[0]
+        val listB = input[1]
+
+        listA.sort()
+        listB.sort()
+
+        for (i in listA.indices) {
+            val distance = listA[i].toInt() - listB[i].toInt()
+            listDistances.add(abs(distance))
+        }
+
+        return listDistances.sum()
+    }
+
+    fun part2(input: Array<ArrayList<String>>): Int {
+        val listSimilarityScore = ArrayList<Int>()
+
+        val listA = input[0]
+        val listB = input[1]
+
+        listA.stream().forEach {
+            val a = it
+
+            var count = 0
+            for (i in listB.indices) {
+                if (a == listB[i]) {
+                    count++
+                }
+            }
+            listSimilarityScore.add(a.toInt()  * count)
+        }
+
+        return listSimilarityScore.sum()
+    }
+
+    fun splitInput(input: List<String>): Array<ArrayList<String>> {
         val listA = ArrayList<String>()
         val listB = ArrayList<String>()
-        val listDistances = ArrayList<Int>()
 
         input.stream().map { it.split("\\s+".toRegex()) }.forEach {
             listA.add(it[0])
             listB.add(it[1])
         }
 
-        listA.sort()
-        listB.sort()
-
-
-        for (i in input.indices) {
-            val distance = listA[i].toInt() - listB[i].toInt()
-            listDistances.add(Math.abs(distance))
-        }
-
-        return listDistances.sum()
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
+        return arrayOf(listA, listB)
     }
 
     // Test if implementation meets criteria from the description, like:
@@ -30,10 +56,11 @@ fun main() {
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 11)
+    check(part1(splitInput(testInput)) == 11)
+    check(part2(splitInput(testInput)) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
-    part1(input).println()
-    //part2(input).println()
+    part1(splitInput(input)).println()
+    part2(splitInput(input)).println()
 }
