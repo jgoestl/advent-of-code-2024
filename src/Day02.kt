c5b199efb842b9127196ce818a8a726e36e5bbce
julian <Em14aAb31h461t4>
2024-12-02 20:19:47 +0100

    Day 02 Part 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -9,42 +9,7 @@ fun main() {
         var safeCount = 0
 
         reports.forEach() {
-            var isSafe = false;
-            var isDecreasing: Boolean? = null
-
-            val report = splitReport(it)
-            for (i in report.indices) {
-                if (i < report.size - 1) {
-                    val x = report[i].toInt() - report[i + 1].toInt()
-                    if (x > 0) {
-                        if (isDecreasing == null) {
-                            isDecreasing = true
-                        } else {
-                            if (!isDecreasing) {
-                                isSafe = false
-                                break
-                            }
-                        }
-                    } else {
-                        if (isDecreasing == null) {
-                            isDecreasing = false
-                        } else {
-                            if (isDecreasing) {
-                                isSafe = false
-                                break
-                            }
-                        }
-                    }
-                    if (abs(x) in 1..3) {
-                        isSafe = true
-                    } else {
-                        isSafe = false
-                        break
-                    }
-                }
-            }
-
-            if (isSafe) {
+            if (checkSafeReport(splitReport((it)), true)) {
                 safeCount++
             }
         }
@@ -52,8 +17,15 @@ fun main() {
         return safeCount
     }
 
-    fun part2(input: Array<ArrayList<String>>): Int {
-        return 0
+    fun part2(reports: List<String>): Int {
+        var safeCount = 0
+
+        reports.forEach() {
+            if (checkSafeReport(splitReport((it)))) {
+                safeCount++
+            }
+        }
+        return safeCount
     }
 
 
@@ -63,10 +35,77 @@ fun main() {
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day02_test")
     check(part1(testInput) == 2)
-    //check(part2(testInput) == 4)
+    check(part2(testInput) == 4)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day02")
     part1(input).println()
-    //part2(input).println()
+    part2(input).println()
+}
+
+fun checkSafeReport(report: List<String>, isUnsafeLevelRemoved: Boolean = false): Boolean {
+    var isSafe = false
+    var isDecreasing: Boolean? = null
+
+    for (i in report.indices) {
+        if (i < report.size - 1) {
+            val x = report[i].toInt() - report[i + 1].toInt()
+            if (x > 0) {
+                if (isDecreasing == null) {
+                    isDecreasing = true
+                } else {
+                    if (isDecreasing == false) {
+                        if (!isUnsafeLevelRemoved) {
+                            return removeUnsafeLevel(report, i)
+                        } else {
+                            isSafe = false
+                            break
+                        }
+                    }
+                }
+            } else if (x < 0) {
+                if (isDecreasing == null) {
+                    isDecreasing = false
+                } else {
+                    if (isDecreasing == true) {
+                        if (!isUnsafeLevelRemoved) {
+                            return removeUnsafeLevel(report, i)
+                        } else {
+                            isSafe = false
+                            break
+                        }
+                    }
+                }
+            }
+            if (abs(x) in 1..3) {
+                isSafe = true
+            } else {
+                if (!isUnsafeLevelRemoved) {
+                    return removeUnsafeLevel(report, i)
+                } else {
+                    isSafe = false
+                    break
+                }
+            }
+        }
+    }
+
+    return isSafe
+}
+
+private fun removeUnsafeLevel(report: List<String>, i: Int): Boolean {
+    var newReport = report.toMutableList()
+    newReport.removeAt(i)
+    var result = checkSafeReport(newReport, true)
+    if (!result && i < report.size - 1) {
+        newReport = report.toMutableList()
+        newReport.removeAt(i + 1)
+        result = checkSafeReport(newReport, true)
+    }
+    if (!result && i > 0) {
+        newReport = report.toMutableList()
+        newReport.removeAt(i - 1)
+        result = checkSafeReport(newReport, true)
+    }
+    return result
 }
